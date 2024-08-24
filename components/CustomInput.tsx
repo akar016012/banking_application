e747cb14d7bebf639bd18e86879edee026c22063
Aditya Kar <61394999+akar016012@users.noreply.github.com>
2024-08-24 17:11:40 -0400

    implemented sign in and sign up form

---
+++ components/CustomInput.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Control, FieldPath } from "react-hook-form";
+import { z } from "zod";
+import { authFormSchema } from "@/lib/utils";
+
+
+const CustomInput = ({
+  control,
+  name,
+  label,
+  placeholder,
+}: {
+  control: Control<z.infer<typeof authFormSchema>>;
+  name: FieldPath<z.infer<typeof authFormSchema>>;
+  label: string;
+  placeholder: string;
+}) => {
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <div className="form-item">
+          <FormLabel className="form-label">{label}</FormLabel>
+          <div className="w-full flex flex-col">
+            <FormControl>
+              <Input
+                placeholder={placeholder}
+                className="input-class"
+                type={name === 'password'?'password':'text'}
+                {...field}
+              ></Input>
+            </FormControl>
+            <FormMessage className="form-message mt-2" />
+          </div>
+        </div>
+      )}
+    />
+  );
+};
+
+export { CustomInput };
